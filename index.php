fed8a61e8f1eae8526c7e8d4b8e7a2144f173b2b
marikadiblasio <marbla.albram@gmail.com>
2023-05-17 14:46:31 +0200

    scaffolding

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <title>BoolZoo</title>
+</head>
+<body>
+        <header class="bg-success">
+            <h1 class="text-center display-4 text-white mb-0"><i class="fa-solid fa-paw"></i> BoolZoo</h1>
+        </header>
+        <main">
+            <div class="container border border-success-subtle">
+                <div class="row">
+                    <div class="col-12 col-sm-6 col-md-4 col-lg-3">
+                        <div class="card">Card</div>
+                    </div>
+                </div>
+            </div>
+        </main>
+        <footer  class="bg-success text-center">
+            <span class="text-white">Made with &hearts; by Boolean</span>
+        </footer>
+</body>
+</html>
